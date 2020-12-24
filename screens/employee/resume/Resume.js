befa34ff09d5a412ea2738b419d7d8036d1568c3
Bryan Hadaway <treatmentrecordsolutions@gmail.com>
2020-12-23 20:33:19 -0500

    added jobs to resume page

--- screens/employee/resume/Resume.js
+++ screens/employee/resume/Resume.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Image, ScrollView } from "react-native";
 import Header from "../../../components/header/Header";
 import { Title, Subheading, Paragraph } from "react-native-paper";
 
 const Resume = () => {
+  const [pastJobs] = useState([
+    {
+      months: "SEPTEMBER 2013 - APRIL 2015",
+      jobTitle: "Senior Web Developer",
+      company: "Snapchat Inc.",
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+      enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat.`,
+    },
+    {
+      months: "SEPTEMBER 2013 - APRIL 2015",
+      jobTitle: "Senior Web Developer",
+      company: "Snapchat Inc.",
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+      enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat.`,
+    },
+    {
+      months: "SEPTEMBER 2013 - APRIL 2015",
+      jobTitle: "Senior Web Developer",
+      company: "Snapchat Inc.",
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+      enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat.`,
+    },
+    {
+      months: "SEPTEMBER 2013 - APRIL 2015",
+      jobTitle: "Senior Web Developer",
+      company: "Snapchat Inc.",
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+      enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat.`,
+    },
+  ]);
+
   return (
     <ScrollView style={styles.peopleWhoLikedYou}>
       <View style={styles.container}>
@@ -44,6 +83,57 @@ const Resume = () => {
             <View style={styles.availabilitySection}>
               <Title style={styles.name}>NOW</Title>
               <Subheading style={styles.jobTitle}>AVAILABILITY</Subheading>
+            </View>
+          </View>
+          {/* hard code first and last job because of the white line */}
+          <View style={styles.pastJobsSection}>
+            <View style={styles.specificPastJob}>
+              <View style={styles.firstVerticalLineSection}>
+                <View style={styles.lastVerticalLine}></View>
+              </View>
+              <View style={styles.jobDetailsSection}>
+                <Subheading style={styles.monthsText}>
+                  {pastJobs[0].months}
+                </Subheading>
+                <Title style={styles.jobTitleText}>
+                  {pastJobs[0].jobTitle}
+                </Title>
+                <Subheading style={styles.companyNameText}>
+                  {pastJobs[0].company}
+                </Subheading>
+              </View>
+            </View>
+            {pastJobs.map((item, i) => (
+              <View style={styles.specificPastJob} key={i}>
+                <View style={styles.verticalLineSection}>
+                  <View style={styles.verticalLine}></View>
+                </View>
+                <View style={styles.jobDetailsSection}>
+                  <Subheading style={styles.monthsText}>
+                    {item.months}
+                  </Subheading>
+                  <Title style={styles.jobTitleText}>{item.jobTitle}</Title>
+                  <Subheading style={styles.companyNameText}>
+                    {item.company}
+                  </Subheading>
+                </View>
+              </View>
+            ))}
+            <View style={styles.specificPastJob}>
+              <View style={styles.lastVerticalLineSection}>
+                <View style={styles.lastVerticalLine}></View>
+              </View>
+              <View style={styles.jobDetailsSection}>
+                <Subheading style={styles.monthsText}>
+                  {pastJobs[pastJobs.length - 1].months}
+                </Subheading>
+                <Title style={styles.jobTitleText}>
+                  {pastJobs[pastJobs.length - 1].jobTitle}
+                </Title>
+                <Subheading style={styles.companyNameText}>
+                  {pastJobs[pastJobs.length - 1].company}
+                </Subheading>
+              </View>
             </View>
           </View>
         </View>
@@ -101,6 +191,64 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  specificPastJob: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  firstVerticalLineSection: {
+    height: "100%",
+    width: 70,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  verticalLineSection: {
+    height: "100%",
+    width: 70,
+    display: "flex",
+    alignItems: "center",
+  },
+  lastVerticalLineSection: {
+    height: "100%",
+    width: 70,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  firstVerticalLine: {
+    height: "50%",
+    width: 5,
+    backgroundColor: "white",
+    borderRadius: 99,
+  },
+  verticalLine: {
+    height: "100%",
+    width: 5,
+    backgroundColor: "white",
+    borderRadius: 99,
+  },
+  lastVerticalLine: {
+    height: "50%",
+    width: 5,
+    backgroundColor: "white",
+    borderRadius: 99,
+  },
+  jobDetailsSection: {
+    flex: 1,
+    display: "flex",
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+  },
+  monthsText: {},
+  jobTitleText: {
+    marginTop: 0,
+    marginBottom: 0,
+    lineHeight: 20,
+  },
+  companyNameText: {
+    marginTop: 0,
+    lineHeight: 20,
   },
 });
 export default Resume;
