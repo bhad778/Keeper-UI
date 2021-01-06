87ccc6fdef693faac7f8e65a00f6dc5abf38c09b
bhad778@gmail.com <treatmentrecordsolutions@gmail.com>
2021-01-06 14:40:47 -0500

    basic profile structure completed

--- screens/profile/Profile.js
+++ screens/profile/Profile.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { Avatar } from "react-native-paper";
+import { Avatar, Button } from "react-native-paper";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import Header from "../../components/header/Header";
 import Settings from "../../modals/Settings";
@@ -28,30 +28,49 @@ const Profile = () => {
       />
       <Header screenTitle="Design Director" withEditButton />
       <View style={styles.profileSection}>
-        <View style={styles.imageContainer}>
+        <View style={styles.imageSection}>
+          <Avatar.Image
+            size={200}
+            source={{
+              uri:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSiVdszvNlg3PRHDEPCs6tRus26-XeBh0a5lQ&usqp=CAU",
+            }}
+            style={styles.images}
+          />
+
           <TouchableOpacity
-            style={styles.imageButton}
-            onPress={() => setProfileViewModalVisible(true)}
-          >
-            <Avatar.Image
-              size={150}
-              source={{
-                uri:
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSiVdszvNlg3PRHDEPCs6tRus26-XeBh0a5lQ&usqp=CAU",
-              }}
-              style={styles.images}
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.editModalButtonContainer}>
-          <TouchableOpacity
-            style={styles.editModalButton}
+            style={styles.editModalButtonContainer}
             onPress={() => setProfileEditModalVisible(true)}
           >
-            <Icon name="create" size={30} />
+            <Icon style={styles.editModalButton} name="create" size={30} />
+            <Text style={styles.profileImageText}>Tap to edit profile</Text>
           </TouchableOpacity>
         </View>
-        <Text style={styles.profileImageText}>Tap to edit profile</Text>
+
+        <View>
+          <View style={styles.row}>
+            <Text style={styles.text}>Preferences</Text>
+            <Icon name="create" color="#b1b1b1" size={30} />
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.text}>Account</Text>
+            <Icon name="create" color="#b1b1b1" size={30} />
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.text}>Help Center</Text>
+            <Icon name="create" color="#b1b1b1" size={30} />
+          </View>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            style={styles.button}
+            mode="contained"
+            color="#ff8365"
+            onPress={() => console.log("Button with adjusted color pressed")}
+          >
+            Get Pare Pro
+          </Button>
+        </View>
       </View>
     </View>
   );
@@ -65,17 +84,30 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
   },
   editModalButtonContainer: {
-    width: 60,
-    height: 60,
+    width: 150,
+    height: 150,
     backgroundColor: "white",
     position: "relative",
-    left: 115,
-    top: -60,
-    borderRadius: 40,
+    bottom: 50,
+    left: 1,
+    borderRadius: 75,
   },
-  editModalButton: { position: "relative", left: 7, top: 6 },
-  settingsButton: {},
-  profileSection: { flex: 8, justifyContent: "flex-start" },
+  text: { color: "#b6b6b6" },
+  editModalButton: {
+    position: "relative",
+    left: 55,
+    top: 10,
+    color: "#b1b1b1",
+  },
+  buttonContainer: {
+    alignItems: "center",
+    width: "100%",
+    position: "relative",
+    top: 30,
+  },
+  button: { width: "40%", borderRadius: 20 },
+  profileSection: { flex: 8, justifyContent: "center", width: "100%" },
+  imageSection: { alignItems: "center" },
   imageContainer: {
     position: "relative",
     top: 15,
@@ -89,8 +121,15 @@ const styles = StyleSheet.create({
     margin: 10,
     marginLeft: 10,
   },
-
-  profileImageText: { position: "relative", bottom: 50, left: 40 },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    margin: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f0f0f0",
+  },
+  profileImageText: { position: "relative", top: 30, left: 15 },
 });
 
 export default Profile;
